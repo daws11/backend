225b9807e441262d675dde22c4cd1859e429ef05
daws11 <dawskutel@gmail.com>
2025-01-22 02:01:23 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: 'wu7nft.stackhero-network.com',
+  port: '3973',
   user: 'root',
-  password: '',
+  password: 'QiWUcSevVC3S7PLytpkT5CZcxGEQibMb',
   database: 'project_management'
 });
 
